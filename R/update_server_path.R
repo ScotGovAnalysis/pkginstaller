34e2d0d143af52ad6e0b94a4b156db78ebbd9088
tomwilsonsco <thomaswilson81@gmail.com>
2023-01-26 10:17:10 +0000

    missing doc

--- R/update_server_path.R
+++ R/update_server_path.R
@@ -1,11 +1,12 @@
-#' Set server path containing packages to install.
+#' Set internal server path containing packages to install.
 #'
 #' This is only needed if get_server_path() shows current path needs changing.
 #' It will only need to be set once as the path specified will be stored (
 #' inside the package install) for all subsequent uses of the
 #' package_installer() function.
 #'
-#' @param server_path
+#' @param server_path string specifying new server path containing packages
+#' (UNC format path e.g. //my_internal_server/my_directory)
 #'
 #' @export
 #'
