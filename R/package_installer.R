612583618cd4744713f406d8fefadd36b5e4138c
tomwilsonsco <thomaswilson81@gmail.com>
2023-01-26 10:10:18 +0000

    remove split out function

--- R/package_installer.R
+++ R/package_installer.R
@@ -1,21 +1,3 @@
-get_server_path <- function() {
-  tryCatch(
-    {
-      server_path <- readLines("packages_server_path.txt")[[1]]
-    },
-    error = function(e) {
-      stop("cannot find packages_server_path")
-    }
-  )
-  if (nchar(server_path) == 0) {
-    stop("no packages server specified in 'packages_server_path.txt'")
-  } else if (substr(server_path, 0, 5) != "file:") {
-    server_path <- paste0("file:", server_path)
-  }
-  return(server_path)
-}
-
-
 #' Install R package from internal server
 #'
 #' To be used in R Studio, optionally as an add-in. A pop-up window will ask
