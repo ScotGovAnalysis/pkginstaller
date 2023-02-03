4292e011deaa93c32ac937928def07b0afebbe0b
tomwilsonsco <thomaswilson81@gmail.com>
2023-02-03 11:53:41 +0000

    Package names

--- R/package_installer.R
+++ R/package_installer.R
@@ -29,7 +29,7 @@ package_installer <- function() {
   pkgs_server <- get_server_path()
 
   pkg_name <- rstudioapi::showPrompt(
-    "Enter Package Name(s)",
+    "Enter Package Name(s) - Separate by commas if multiple",
     paste(
       "Install from",
       substr(pkgs_server, 6, nchar(pkgs_server))
